f55524d04940fc8a4ab56f48b800ff6f9db6e29c
Niteshkumar-codes <nitesh.connect242@gmail.com>
2026-04-22 22:39:29 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,71 @@
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  let [todolist, setTodolist] = useState([]);
+
+  let saveToDoList = (event) => {
+    event.preventDefault();
+
+    let toname = event.target.toname.value;
+
+    if (!todolist.includes(toname)) {
+      let finalDolist = [...todolist, toname];
+      setTodolist(finalDolist);
+    } else {
+      alert("ToDo Name Already Exists...");
+    }
+  };
+
+  let list = todolist.map((value, index) => {
+    return (
+      <ToDoListItems
+        value={value}
+        key={index}
+        indexNumber={index}
+        todolist={todolist}
+        setTodolist={setTodolist}
+      />
+    );
+  });
+
+  return (
+    <div className="App">
+      <h1>ToDo List</h1>
+
+      <form onSubmit={saveToDoList}>
+        <input type="text" name="toname" />
+        <button>Save</button>
+      </form>
+
+      <div className="outerDiv">
+        <ul>{list}</ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+function ToDoListItems({ value, indexNumber, todolist, setTodolist }) {
+  let [status, setStatus] = useState(false);
+
+  let deleteRow = () => {
+    let finalData = todolist.filter((v, i) => i !== indexNumber);
+    setTodolist(finalData);
+  };
+
+  let checkStatus = () => {
+    setStatus(!status);
+  };
+
+  return (
+    <li
+      className={status ? "completetodo" : ""}
+      onClick={checkStatus}
+    >
+      {value}
+      <span onClick={deleteRow}>&times;</span>
+    </li>
+  );
+}
